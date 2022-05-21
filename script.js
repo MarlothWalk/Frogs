81752fe52788a4b9a102a4811467bbc10f749d14
MarlothWalk <54508374+MarlothWalk@users.noreply.github.com>
2022-05-21 02:18:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+function playCall(href)
+{
+	if (calls[href])
+	{
+		calls[href].play()		
+	}
+}
+function pauseCall(href)
+{
+	if (calls[href])
+	{
+		calls[href].pause()		
+	}
+}
+let calls = {}
+function loadCalls()
+{
+	$('button.play').each(function(){
+		let href = $(this).attr('href')
+		if (href)
+		{
+			calls[href] = new Audio(href)
+			calls[href].loop = true
+			calls[href].load()
+		}
+	});
+}
+function setYoutubeLinks()
+{
+	$('button.youtube').click(function(){
+		let href = $(this).attr('href');
+
+	})
+}
+function setLangLinks()
+{
+
+	$('.languagepicker').change(function() {		
+		lang = $(this).val()
+		console.log(lang)
+		link = "../"+lang+"/index.html"
+		window.location.href = link
+	})
+}
+$(document).ready(() => {
+	setLangLinks()
+	loadCalls()
+	setYoutubeLinks()
+	$('button.play').click(function() { 
+		let href = $(this).attr('href')
+		let playing = $(this).attr('playing');
+		if (playing == "true")
+		{
+			pauseCall(href)		
+		}
+		else
+		{
+			playCall(href) 
+		}
+		$(this).toggleClass('fa-play')
+		$(this).toggleClass('fa-pause')
+		playing = playing == 'true' ? 'false':'true'
+		$(this).attr('playing',playing)		
+		$(this).text(playing == 'true' ? 'Pause Call' : 'Play Call')
+	})
+});
